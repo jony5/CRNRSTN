32702ab21c1d738833c1ad6cd2c391add7618e61
Jonathan 'J5' Harris <jharris@evifweb.com>
2018-06-19 09:43:25 -0400

    Update _crnrstn.db.config.inc.php
    Comment updates.

--- config.database.secure/_crnrstn.db.config.inc.php
+++ config.database.secure/_crnrstn.db.config.inc.php
@@ -5,9 +5,10 @@
 
 //
 // INITIALIZE DATABASE FUNCTIONALITY FOR EACH ENVIRONMENT.
-# IF CENTRALIZED DATABASE CONFIG MGMT IS NOT YOUR THING, YOU CAN PASS ALL 
-# DATABASE AUTH CONFIG PARAMETERS TO $oCRNRSTN_ENV->oMYSQLI_CONN_MGR->returnConnection()  WHEN
-# CREATING A MYSQLI DATABASE CONNECTION OBJECT. SEE http://crnrstn.evifweb.com/documentation/classes/mysqli_conn_manager/returnconnection/
+# IF CENTRALIZED AND ENVIRONMENTALLY AWARE DATABASE CONFIG MGMT IS NOT YOUR THING, YOU CAN 
+# PASS ALL DATABASE AUTH CONFIG PARAMETERS TO $oCRNRSTN_ENV->oMYSQLI_CONN_MGR->returnConnection()  WHEN
+# CREATING A MYSQLI DATABASE CONNECTION OBJECT WITHIN YOUR APPLICATION.
+# SEE http://crnrstn.evifweb.com/documentation/classes/mysqli_conn_manager/returnconnection/
 #
 # $this->oMYSQLI_CONN_MGR->addConnection([environment-key], [db-host], [db-user-name], [db-user-pswd], [db-database-name], [optional-db-port]);
 # $this->oMYSQLI_CONN_MGR->addConnection('LOCALHOST_PC', '127.0.0.4', 'crnrstn_demo3_un', 'FZZ88X3EU5s8vFAC', 'crnrstn_demo3');
