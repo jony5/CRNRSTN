f9f9a272918dd6cd4a9ba5a89a3ba74d492a032f
Jonathan 'J5' Harris <jharris@evifweb.com>
2018-07-25 11:27:46 -0400

    Add files via upload
    Mainly formatting updates.

--- _crnrstn/class/crnrstn/crnrstn.inc.php
+++ _crnrstn/class/crnrstn/crnrstn.inc.php
@@ -9,26 +9,26 @@
 #  AUTHOR :: Jonathan 'J5' Harris, Lead Full Stack Developer
 #  URI :: http://crnrstn.evifweb.com/
 #  OVERVIEW :: CRNRSTN is an open source PHP class library that facilitates the operation of an application within multiple server 
-#			   environments (e.g. localhost, stage, preprod, and production). With this tool, data and functionality with 
-#			   characteristics that inherently create distinctions from one environment to the next...such as IP address restrictions, 
-#			   error logging profiles, and database authentication credentials...can all be managed through one framework for an entire 
-#			   application. Once CRNRSTN has been configured for your different hosting environments, seamlessly release a web 
-#			   application from one environment to the next without having to change your code-base to account for environmentally 
-#			   specific parameters; and manage this all from one place within the CRNRSTN Suite ::
+#		environments (e.g. localhost, stage, preprod, and production). With this tool, data and functionality with 
+#		characteristics that inherently create distinctions from one environment to the next...such as IP address restrictions, 
+#		error logging profiles, and database authentication credentials...can all be managed through one framework for an entire 
+#		application. Once CRNRSTN has been configured for your different hosting environments, seamlessly release a web 
+#		application from one environment to the next without having to change your code-base to account for environmentally 
+#		specific parameters; and manage this all from one place within the CRNRSTN Suite ::
 
 #  MIT LICENSE :: Copyright 2018 Jonathan J5 Harris
-#			  	  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-#				  documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
-#				  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
-#				  permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#		Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+#		documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+#		rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+#		permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-#			  	  The above copyright notice and this permission notice shall be included in all copies or substantial portions 
-#				  of the Software.
+#		The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+#		of the Software.
 
-#			  	  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
-#			  	  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
-#				  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
-#				  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+#		WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+#		OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+#		OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 /*
 // CLASS :: crnrstn
