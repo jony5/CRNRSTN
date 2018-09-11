4ef862b7fa2a23e39454e26a81bcfc211f17e2ae
Jonathan 'J5' Harris <jharris@evifweb.com>
2018-09-11 19:43:19 -0400

    Update crnrstn_config_debug.php

--- crnrstn_config_debug.php
+++ crnrstn_config_debug.php
@@ -4,27 +4,31 @@
 // Code is Poetry */
 #  CRNRSTN Suite :: An Open Source PHP Class Library to facilitate the operation of an application across multiple hosting environments.
 #  Copyright (C) 2012-2018 eVifweb Development
-#  VERSION :: 1.0.0
-#  RELEASE DATE :: July 4, 2018 - Happy Independence Day from my dog and I to you...wherever and whenever you are.
+#  VERSION :: 1.0.1
+#  RELEASE DATE (v1.0.0) :: July 4, 2018 - Happy Independence Day from my dog and I to you...wherever and whenever you are.
 #  AUTHOR :: Jonathan 'J5' Harris, Lead Full Stack Developer
 #  URI :: http://crnrstn.evifweb.com/
 #  OVERVIEW :: CRNRSTN is an open source PHP class library that facilitates the operation of an application within multiple server 
-#			   environments (e.g. localhost, stage, preprod, and production). With this tool, data and functionality with 
-#			   characteristics that inherently create distinctions from one environment to the next...such as IP address restrictions, 
-#			   error logging profiles, and database authentication credentials...can all be managed through one framework for an entire 
-#			   application. Once CRNRSTN has been configured for your different hosting environments, seamlessly release a web 
-#			   application from one environment to the next without having to change your code-base to account for environmentally 
-#			   specific parameters; and manage this all from one place within the CRNRSTN Suite ::
+#		environments (e.g. localhost, stage, preprod, and production). With this tool, data and functionality with 
+#		characteristics that inherently create distinctions from one environment to the next...such as IP address restrictions, 
+#		error logging profiles, and database authentication credentials...can all be managed through one framework for an entire 
+#		application. Once CRNRSTN has been configured for your different hosting environments, seamlessly release a web 
+#		application from one environment to the next without having to change your code-base to account for environmentally 
+#		specific parameters; and manage this all from one place within the CRNRSTN Suite ::
 
-#  LICENSE :: This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public 
-#			  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any 
-#			  later version.
-#
-#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
-#  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along with this program. This license can also be downloaded from
-#  http://crnrstn.evifweb.com/license.txt.  If not, see <http://www.gnu.org/licenses/>
+#  MIT LICENSE :: Copyright 2018 Jonathan J5 Harris
+#		Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+#		documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+#		rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+#		permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+#		The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+#		of the Software.
+
+#		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+#		WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+#		OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+#		OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 require('_crnrstn.root.inc.php');
 include_once($CRNRSTN_ROOT . '_crnrstn.config.inc.php');
@@ -106,27 +110,27 @@ $tmp_crnrstnART[3] = '
 <meta charset="utf-8" />
 <title>CRNRSTN Suite :: Configuration Debug</title>
 <style type="text/css">
-	body						{ margin:0;font-size:.7em; font-family:Arial, Helvetica, sans-serif; background:#EEE;}
-	#content_wrapper			{ width:100%; text-align:center; margin:0px auto;}
-	#mini_nav_wrapper			{ width:590px; text-align:center; margin:0px auto;}
-	.mini_nav_lnk				{ float:left; padding-right:9px;}
-	.topAnchor_lnk				{ padding:15px; float:right;}
-	#content_main				{ width:800px; text-align:center; margin:0px auto;}
-	#page_title					{ font-size:25px; padding-bottom:10px; padding-top:5px;}
-	#sess_clear_link			{ text-align:center; margin:0px auto;}
-	#sess_id					{ font-family:"Courier New", Courier, monospace; padding-top:5px;}
-	.the_R						{ color:#F00;}
-	.hr							{ width:90%; text-align:center; margin:0px auto; border-bottom:2px dashed #06F;}
-	.svr_elem_scroll			{ height:200px; overflow-y:scroll;}
+	body				{ margin:0;font-size:.7em; font-family:Arial, Helvetica, sans-serif; background:#EEE;}
+	#content_wrapper		{ width:100%; text-align:center; margin:0px auto;}
+	#mini_nav_wrapper		{ width:590px; text-align:center; margin:0px auto;}
+	.mini_nav_lnk			{ float:left; padding-right:9px;}
+	.topAnchor_lnk			{ padding:15px; float:right;}
+	#content_main			{ width:800px; text-align:center; margin:0px auto;}
+	#page_title			{ font-size:25px; padding-bottom:10px; padding-top:5px;}
+	#sess_clear_link		{ text-align:center; margin:0px auto;}
+	#sess_id			{ font-family:"Courier New", Courier, monospace; padding-top:5px;}
+	.the_R				{ color:#F00;}
+	.hr				{ width:90%; text-align:center; margin:0px auto; border-bottom:2px dashed #06F;}
+	.svr_elem_scroll		{ height:200px; overflow-y:scroll;}
 	#server_param_overview		{ font-size:13px; text-align:left; padding:5px 20px 10px 20px; line-height:19px;}
 	#server_params_wrapper		{ border:3px solid #F00; width:750px; text-align:center; margin:0px auto;}
-	.server_param				{ text-align:left; padding:5px 10px 2px 20px;}
-	.server_param_best			{ text-align:left; padding:5px 10px 2px 20px; font-weight:bold; color:#900; font-size:13px;}
-	.red						{ font-weight:bold; color:#900; font-size:13px;}
-	.copy_wrapper				{ width:750px; text-align:center; margin:0px auto;}
-	.copy						{ text-align:left; padding:0 10px 10px 10px; font-size:13px; line-height:18px;}
-	.underL						{ text-decoration:underline;}
-	.crnrstn_param				{ text-align:left; padding:5px 10px 12px 20px;}
+	.server_param			{ text-align:left; padding:5px 10px 2px 20px;}
+	.server_param_best		{ text-align:left; padding:5px 10px 2px 20px; font-weight:bold; color:#900; font-size:13px;}
+	.red				{ font-weight:bold; color:#900; font-size:13px;}
+	.copy_wrapper			{ width:750px; text-align:center; margin:0px auto;}
+	.copy				{ text-align:left; padding:0 10px 10px 10px; font-size:13px; line-height:18px;}
+	.underL				{ text-decoration:underline;}
+	.crnrstn_param			{ text-align:left; padding:5px 10px 12px 20px;}
 	
 	#crnrstn_params_wrapper		{ border:3px solid #06F; width:750px; text-align:center; margin:0px auto;}
 	#crnrstn_param_overview		{ font-size:13px; text-align:left; padding:5px 20px 0 20px; line-height:19px;}
@@ -140,16 +144,16 @@ $tmp_crnrstnART[3] = '
 	#crnrstn_hash_wrapper		{ border:3px solid #00FFF7; width:750px; text-align:center; margin:0px auto;}
 	#crnrstn_hash_overview		{ font-size:13px; text-align:left; padding:5px 20px 0 20px; line-height:19px;}
 	
-	#crnrstn_db_wrapper			{ border:3px solid #F100DB; width:750px; text-align:center; margin:0px auto;}
+	#crnrstn_db_wrapper		{ border:3px solid #F100DB; width:750px; text-align:center; margin:0px auto;}
 	#crnrstn_db_overview		{ font-size:13px; text-align:left; padding:5px 20px 0 20px; line-height:19px;}
 	
-	.debug_output				{ font-size:10px;height:400px;overflow:scroll;border-bottom:2px solid #333;padding-bottom:10px;}
+	.debug_output			{ font-size:10px;height:400px;overflow:scroll;border-bottom:2px solid #333;padding-bottom:10px;}
 	
-	#ftr_cw						{ text-align:center; font-size:11px; color:#333; padding:20px 0 30px 0;}
+	#ftr_cw				{ text-align:center; font-size:11px; color:#333; padding:20px 0 30px 0;}
 	
-	.cb_5						{ display:block; clear:both; height:5px;  line-height:1px; overflow:hidden; border:0px; padding:0px; margin:0px; font-size:1px;}
-	.cb_10						{ display:block; clear:both; height:10px;  line-height:1px; overflow:hidden; border:0px; padding:0px; margin:0px; font-size:1px;}
-	.cb_20						{ display:block; clear:both; height:20px;  line-height:1px; overflow:hidden; border:0px; padding:0px; margin:0px; font-size:1px;}
+	.cb_5				{ display:block; clear:both; height:5px;  line-height:1px; overflow:hidden; border:0px; padding:0px; margin:0px; font-size:1px;}
+	.cb_10				{ display:block; clear:both; height:10px;  line-height:1px; overflow:hidden; border:0px; padding:0px; margin:0px; font-size:1px;}
+	.cb_20				{ display:block; clear:both; height:20px;  line-height:1px; overflow:hidden; border:0px; padding:0px; margin:0px; font-size:1px;}
 </style>
 </head>
 
